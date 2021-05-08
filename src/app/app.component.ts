57bdac02b0f06af0920a3e07cb68b2263badcf9f
harsh <agrawalh495@gmail.com>
2021-05-08 09:26:57 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'like-comp-project';
+
+  tweet = {
+    isLiked: true,
+    likesCount: 10
+  }
+
 }
--- src/app/like/like.component.ts
+++ src/app/like/like.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-like',
   templateUrl: './like.component.html',
   styleUrls: ['./like.component.css']
 })
+
+
 export class LikeComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  @Input() isActive: boolean;
+  @Input() likesCount: number;
+
+  onClick(){
+    this.likesCount+= (this.isActive) ? -1 : 1;
+    this.isActive = !this.isActive;
+    console.log(this.isActive);
   }
 
 }
